806f53093792d068afc6e1a3018a5919e17c93ad
kelly r maass <kmaass77627@gmail.com>
2020-09-22 04:25:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function fetchBooks() {
 
+
+function fetchBooks() {
+return fetch('https://anapioficeandfire.com/api/books')
+.then(function(response) {
+  return response.json();
+})
+.then(function(json){
+  renderBooks(json)
+})
 }
 
 function renderBooks(books) {
